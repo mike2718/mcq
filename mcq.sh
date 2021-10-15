555bb260f6d220b8c78a88b863b24bf8711c7e04
Mike Akiba <mike2718@gmail.com>
2021-10-15 20:32:44 +0800

    update

--- mcq.sh
+++ mcq.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/zsh
 #
 clear
-printf "\n-------------------------------\n"
+printf "\n\n"
 printf "     随机选择题选项生成器\n"
 printf "           ver 0.2\n"
-printf "-------------------------------\n\n"
-r=("A" "B" "C" "D")
+printf "\n\n"
+r=(A B C D)
 printf "打乱前\t"
-printf "%-5s" "${r[@]}"
+printf '%c' "${r[@]}"
 printf "\n"
 for i in 4 3 2 1
 do
      # 生成一个j,使得1 ≤ j ≤ i
-     j=$(head /dev/urandom | tr -dc 1-4 | head -c1)
+     j="$(head /dev/urandom | tr -dc 1-4 | head -c1)"
      until [[ "$j" -le "$i" ]] && [[ "$j" -ge 1 ]]
      do
-        j=$(head /dev/urandom | tr -dc 1-4 | head -c1)
+        j="$(head /dev/urandom | tr -dc 1-4 | head -c1)"
      done
      # 交换r[i]和r[j]
      tmp="${r[j]}"
@@ -23,6 +23,5 @@ do
      r[i]="$tmp"
 done
 printf "打乱后\t"
-printf "%-5s" "${r[@]}"
+printf '%c' "${r[@]}"
 printf "\n\n"
-

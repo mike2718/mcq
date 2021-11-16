6a2b182ba4362abcf03869beb31155d25c9f5d5f
Mike Akiba <mike2718@gmail.com>
2021-11-17 00:50:03 +0800

    update

--- mcq.sh
+++ mcq.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/bash
+#!/usr/bin/zsh
 #
 clear
-printf "\n\n"
+printf "\n"
 printf "     随机选择题选项生成器\n"
 printf "           ver 0.2\n"
 printf "\n\n"
@@ -11,10 +11,10 @@ printf '%c' "${r[@]}"
 printf "\n"
 for i in 4 3 2 1; do
      # 生成一个j,使得1 ≤ j ≤ i
-     j="$(head /dev/urandom | tr -dc 1-4 | head -c1)"
+     j="$(head /dev/random | tr -dc 1-4 | head -c1)"
      until [[ "${j}" -le "${i}" ]] && [[ "${j}" -ge 1 ]]
      do
-        j="$(head /dev/urandom | tr -dc 1-4 | head -c1)"
+        j="$(head /dev/random | tr -dc 1-4 | head -c1)"
      done
      # 交换r[i]和r[j]
      tmp="${r[j]}"

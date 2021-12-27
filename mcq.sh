d30f7ba283aef856cb017dee4a8cb7574c3f254e
Mike Akiba <mike2718@gmail.com>
2021-12-27 17:27:30 +0800

    修复错误的程序

--- mcq.sh
+++ mcq.sh
@@ -1,24 +1,29 @@
-#!/usr/bin/zsh
+#!/usr/bin/bash
 clear
 printf "\t随机选择题选项生成器\n\n"
 r=(A B C D)
-printf "打乱前\t"
-printf '%c' "${r[@]}"
-printf "\n"
-printf "\t↓↓↓↓\n"
-for i in 4 3 2 1; do
-     # 生成一个j,使得1 ≤ j ≤ i
-     j="$(head /dev/random | tr -dc 1-4 | head -c1)"
-     until [[ "${j}" -le "${i}" ]] && [[ "${j}" -ge 1 ]]
-     do
-        j="$(head /dev/random | tr -dc 1-4 | head -c1)"
+#printf "\t↓↓↓↓\n"
+printf "范围\t摇到的数组下标\t划掉的字母\t结果\n"
+printf "\t\t\t"
+printf '%c ' "${r[@]}"
+echo
+for i in 3 2 1; do # 1 <= i <= 4 - 1
+     # 生成一个j, 使得1 ≤ j ≤ i
+     printf '%s%c\t' "1-" "$(( i + 1 ))"
+     j="$(head /dev/random | tr -dc 0-3 | head -c1)"
+     until [[ "${j}" -ge 0 ]] && [[ "${j}" -le "${i}" ]]; do
+        j="$(head /dev/random | tr -dc 0-3 | head -c1)"
      done
      # 交换r[i]和r[j]
      tmp="${r[j]}"
      r[j]="${r[i]}"
      r[i]="${tmp}"
+     printf '%c\t\t' "${j}"
+     printf '%b\t\t' "\033[1m"${r[j]}"\033[0m"
+     printf '%b\n' "\033[1m"${r[i]}"\033[0m"
 done
-printf "打乱后\t"
-printf "%c" "${r[@]}"
-printf "\n\n"
-
+echo
+printf "打乱后的序列："
+printf '%c ' "${r[@]}"
+echo
+echo
